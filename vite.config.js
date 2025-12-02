339766103093d1d8c3bd7c17a784df3e11f4383e
serobless <se.robless@duocuc.cl>
2025-12-01 23:22:20 -0300

    fix: Corrige la ruta base para el deploy en EC2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sabores-chile-react/',
+  base: '/',
   test: {
     globals: true,
     environment: 'happy-dom',
